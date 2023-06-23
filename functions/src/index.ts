55b6db8e84c0480c8b9895d4f14435f5fe0ef481
HieuVM <vuhieu2610@gmail.com>
2023-06-23 22:53:09 +0700

    add request handler for express and typescript compiler configs

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,8 @@
-import { onRequest } from "firebase-functions/v2/https";
-import * as firestore from "firebase-functions/v1/firestore";
-import { initializeApp } from "firebase-admin/app";
-import { getFirestore } from "firebase-admin/firestore";
-import express, { Request, Response } from 'express';
+import * as functions from 'firebase-functions';
+import * as firestore from 'firebase-functions/v1/firestore';
+import { initializeApp } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+import express, { Request, Response, RequestHandler } from 'express';
 import cors from 'cors';
 
 //import * as middleware from './middlewares/index';
@@ -13,11 +13,19 @@ initializeApp();
 const app = express();
 const app2 = express();
 
-app.use(cors({
-  credentials: true,
-}));
+app.use(
+  cors({
+    credentials: true,
+  })
+);
 
 //app.use(middleware.isAuthenticated);
+app.use(express.json() as RequestHandler);
+app.use(
+  express.urlencoded({
+    extended: true,
+  }) as RequestHandler
+);
 app.use('/', router());
 app.get('/hello', (req: Request, res: Response) => {
   res.send('Hello, world!');
@@ -25,11 +33,9 @@ app.get('/hello', (req: Request, res: Response) => {
 app2.get('/hello2', (req: Request, res: Response) => {
   res.send('Hello, world 2!');
 });
+export const paypal = functions.https.onRequest(app);
 
-export const paypal = onRequest(app);
-
-
-export const addMessage = onRequest(async (req, res) => {
+export const addMessage = functions.https.onRequest(async (req, res) => {
   const original = req.query.text;
 
   const writeResult = await getFirestore()
@@ -39,13 +45,15 @@ export const addMessage = onRequest(async (req, res) => {
   res.json({ result: `Message with ID: ${writeResult.id} added.` });
 });
 
-export const makeUppercase = firestore.document('/messages/{documentId}').onCreate(async (snapshot, context) => {
-  const original = snapshot.data()?.original;
-  const documentId = context.params.documentId;
+export const makeUppercase = firestore
+  .document('/messages/{documentId}')
+  .onCreate(async (snapshot, context) => {
+    const original = snapshot.data()?.original;
+    const documentId = context.params.documentId;
 
-  console.log('Uppercasing', documentId, original);
+    console.log('Uppercasing', documentId, original);
 
-  const uppercase = original.toUpperCase();
+    const uppercase = original.toUpperCase();
 
-  return snapshot.ref.set({ uppercase }, { merge: true });
-});
+    return snapshot.ref.set({ uppercase }, { merge: true });
+  });
