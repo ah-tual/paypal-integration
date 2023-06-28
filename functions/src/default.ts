f2dd19efa7fdbc85484320dfa8a5d39d5aeb997c
tuanva <tuanva23@fpt.com.vn>
2023-06-28 17:57:21 +0700

    no msg

--- functions/src/default.ts
+++ functions/src/default.ts
@@ -11,7 +11,7 @@ export const devConfig = {
     paypal: {
         url: 'https://api-m.sandbox.paypal.com',
         ipnUrl: 'https://ipnpb.sandbox.paypal.com',
-        basicAuth: 'Basic QVFlaWpMcXk4UWJVVjNBR2E1QlM4eHJIaXFXT3dYSW42VTdadUdEOUJDTURrdUlXNFhvMVZSTlpOa1BPMGY2NlZpR3FRYU90eENvR0VNQU46RUpIdWxFakxGWGdaQWlwc1B0RmpHN2ZFLUoydExLYzFaZ0Y5MnlsdjMzTTRCeTRTallBVTJLTHVqSFZtOTQzSHRReEd1WkJ1X3p4cnlUZ2Q='
+        basicAuth: 'Basic '
     },
     google: {
         clientId: '351360855136-c65vr13tal2in9b9m1hdmp5dgr4rie3l.apps.googleusercontent.com'
