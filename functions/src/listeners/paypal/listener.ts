338501a0a47490520db001fad9aecd9b3f7f8cfd
tuanva <tuanva23@fpt.com.vn>
2023-06-29 17:39:11 +0700

    no msg

--- functions/src/listeners/paypal/listener.ts
+++ functions/src/listeners/paypal/listener.ts
@@ -1,16 +1,24 @@
 import express from 'express';
 import querystring from 'querystring';
-import  { AxiosResponse, AxiosError } from 'axios';
+import { AxiosResponse, AxiosError } from 'axios';
 import { getFirestore } from "firebase-admin/firestore";
 
 import config from '../../default';
 import { postForm } from '../../helpers/apiClient';
+import { verifySubcription } from '../../usecases/paypal/subscription';
 
 export const handleCallback = async (req: express.Request, res: express.Response) => {
   console.log("Callback Notification Event Received");
   console.log(req.query);
   console.log(req.params);
   console.log(req.body);
+  try {
+    await verifySubcription(<string>req.query.ba_token, <string>req.query.subscription_id, <string>req.query.token);
+    res.status(200).json({ subscriptionId: req.query.subscription_id, 'status': 'ACTIVE' });
+  }
+  catch (ex) {
+    res.status(400);
+  }
 }
 
 export const handleIPN = async (req: express.Request, res: express.Response) => {
@@ -35,25 +43,25 @@ export const handleIPN = async (req: express.Request, res: express.Response) => 
   console.log(`Verifying IPN: ${verificationBody}`);
 
   postForm(`${config.paypal.ipnUrl}/cgi-bin/webscr`, { ...req.body, cmd: `_notify-validate` })
-  .then((res: AxiosResponse) => {
-    console.log('Response:', res.data);
-    if (res.status == 200) {
-      if (res.data === "VERIFIED") {
-        console.log(
-          `Verified IPN: IPN message for Transaction ID: ${ipnTransactionMessage.txn_id} is verified.`
-        );
-      } else if (res.data === "INVALID") {
-        console.error(
-          `Invalid IPN: IPN message for Transaction ID: ${ipnTransactionMessage.txn_id} is invalid.`
-        );
-      } else {
-        console.error("Unexpected reponse body.");
+    .then((res: AxiosResponse) => {
+      console.log('Response:', res.data);
+      if (res.status == 200) {
+        if (res.data === "VERIFIED") {
+          console.log(
+            `Verified IPN: IPN message for Transaction ID: ${ipnTransactionMessage.txn_id} is verified.`
+          );
+        } else if (res.data === "INVALID") {
+          console.error(
+            `Invalid IPN: IPN message for Transaction ID: ${ipnTransactionMessage.txn_id} is invalid.`
+          );
+        } else {
+          console.error("Unexpected reponse body.");
+        }
       }
-    }
-  })
-  .catch((error: AxiosError) => {
-    console.error('Error:', error.message);
-  });
+    })
+    .catch((error: AxiosError) => {
+      console.error('Error:', error.message);
+    });
 
 
   // axios.post(`${config.paypalIPNUri}/cgi-bin/webscr`, verificationBody, {
